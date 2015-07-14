01dc4eedb7d0eea769a49cffac330d272a1b979c
Mattia <mattia.belletti@gmail.com>
2015-07-14 17:31:22 +0200

    Legendaries
    Remaining legendaries added, together with legendary sorting by name.

--- script.js
+++ script.js
@@ -6,20 +6,269 @@ angular.module('legendarySearch', [
 .service('CostComputer', function() {
 	return {
 		costs: {
+			19621: [ // gift of metal
+				{type: 'item', id: 19686, amount: 250},
+				{type: 'item', id: 19681, amount: 250},
+				{type: 'item', id: 19684, amount: 250},
+				{type: 'item', id: 19685, amount: 250},
+			],
+			19622: [ // gift of wood
+				{type: 'item', id: 19714, amount: 250},
+				{type: 'item', id: 19711, amount: 250},
+				{type: 'item', id: 19709, amount: 250},
+				{type: 'item', id: 19712, amount: 250},
+			],
+			19623: [ // gift of energy
+				{type: 'item', id: 24274, amount: 250},
+				{type: 'item', id: 24275, amount: 250},
+				{type: 'item', id: 24276, amount: 250},
+				{type: 'item', id: 24277, amount: 250},
+			],
+			19624: [ // gift of ice
+				{type: 'item', id: 19670, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 24320, amount: 100},
+				{type: 'item', id: 24340, amount: 100}
+			],
+			19625: [ // gift of frostfang
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19624, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24555, amount: 1},
+			],
 			19626: [ // gift of fortune
 				{ type: 'item', id: 19675, amount: 77 },
 				{ type: 'item', id: 19721, amount: 250 },
 				{ type: 'item', id: 19673 },
 				{ type: 'item', id: 19672 }
 			],
+			19627: [ // gift of nature
+				{type: 'item', id: 19667, amount: 1},
+				{type: 'item', id: 12128, amount: 250},
+				{type: 'item', id: 19712, amount: 250},
+				{type: 'item', id: 19737, amount: 250}
+			],
+			19628: [ // unicorn statue
+				{type: 'item', id: 19667, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 24512, amount: 100},
+				{type: 'item', id: 24522, amount: 100}
+			],
+			19629: [ // gift of history
+				{type: 'item', id: 19664, amount: 1},
+				{type: 'item', id: 19737, amount: 250},
+				{type: 'item', id: 24277, amount: 250},
+				{type: 'item', id: 24310, amount: 100}
+			],
+			19630: [ // gift of music
+				{type: 'item', id: 19665, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19746, amount: 250},
+				{type: 'item', id: 24522, amount: 100}
+			],
+			19631: [ // gift of darkness
+				{type: 'item', id: 19664, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19737, amount: 250},
+				{type: 'item', id: 24310, amount: 100}
+			],
+			19633: [ // vial of quicksilver
+				{type: 'item', id: 19666, amount: 1},
+				{type: 'item', id: 19688, amount: 250},
+				{type: 'item', id: 24315, amount: 150},
+				{type: 'item', id: 24502, amount: 250}
+			],
+			19634: [ // vial of liquid flame
+				{type: 'item', id: 19668, amount: 1},
+				{type: 'item', id: 12544, amount: 250},
+				{type: 'item', id: 24315, amount: 100},
+				{type: 'item', id: 24325, amount: 100}
+			],
+			19635: [ // gift of entertainment
+				{type: 'item', id: 19665, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19746, amount: 250},
+				{type: 'item', id: 20000, amount: 5}
+			],
+			19636: [ // gift of stealth
+				{type: 'item', id: 19671, amount: 1},
+				{type: 'item', id: 12545, amount: 250},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 24310, amount: 100}
+			],
+			19637: [
+				{type: 'item', id: 19671, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19732, amount: 250},
+				{type: 'item', id: 24305, amount: 100}
+			],
+			19638: [ // gift of color
+				{type: 'item', id: 19669, amount: 1},
+				{type: 'item', id: 20323, amount: 100},
+				{type: 'item', id: 24277, amount: 250},
+				{type: 'item', id: 24522, amount: 100}
+			],
+			19639: [ // gift of lightning
+				{type: 'item', id: 19664, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19746, amount: 250},
+				{type: 'item', id: 24305, amount: 100}
+			],
+			19641: [ // shark statue
+				{type: 'item', id: 19669, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 24289, amount: 250},
+				{type: 'item', id: 24295, amount: 250}
+			],
+			19642: [ // eel statue
+				{type: 'item', id: 19666, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 19737, amount: 250},
+				{type: 'item', id: 24289, amount: 250}
+			],
+			19643: [ // gift of water
+				{type: 'item', id: 19670, amount: 1},
+				{type: 'item', id: 19685, amount: 250},
+				{type: 'item', id: 24315, amount: 100},
+				{type: 'item', id: 24320, amount: 100}
+			],
+			19644: [ // gift of kudzu
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19627, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24865, amount: 1},
+			],
+			19645: [ // gift of incinerator
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19634, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24548, amount: 1},
+			],
+			19646: [ // gift of the minstrel
+				{type: 'item', id: 19623, amount: 1},
+				{type: 'item', id: 19630, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24607, amount: 1},
+			],
+			19647: [ // gift of sunrise
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19632, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24562, amount: 1},
+			],
+			19648: [ // gift of twilight
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19631, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24570, amount: 1},
+			],
+			19649: [ // gift of the juggernaut
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19633, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24584, amount: 1},
+			],
+			19650: [ // gift of the moot
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19635, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24607, amount: 1},
+			],
+			19651: [ // gift of quip
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19635, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24592, amount: 1},
+			],
+			19652: [ // gift of meteorlogicus
+				{type: 'item', id: 19623, amount: 1},
+				{type: 'item', id: 19637, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24554, amount: 1},
+			],
+			19653: [ // gift of the flameseeker prophecies
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19629, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24601, amount: 1},
+			],
+			19654: [ // gift of the bifrost
+				{type: 'item', id: 19623, amount: 1},
+				{type: 'item', id: 19638, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24572, amount: 1},
+			],
+			19655: [ // gift of bolt
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19639, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24554, amount: 1},
+			],
+			19656: [ // gift of rodgort
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19634, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24548, amount: 1},
+			],
+			19657: [ // gift of Kamohoali'i Kotaki
+				{type: 'item', id: 19621, amount: 1},
+				{type: 'item', id: 19641, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24612, amount: 1},
+			],
+			19658: [ // gift of kraitkin
+				{type: 'item', id: 19623, amount: 1},
+				{type: 'item', id: 19642, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24632, amount: 1},
+			],
+			19659: [ // gift of frenzy
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19643, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24561, amount: 1},
+			],
+			19660: [ // gift of the dreamer
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19628, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24571, amount: 1},
+			],
+			19661: [ // gift of the predator
+				{type: 'item', id: 19622, amount: 1},
+				{type: 'item', id: 19636, amount: 1},
+				{type: 'item', id: 19676, amount: 100},
+				{type: 'item', id: 24615, amount: 1},
+			],
 			19662: [ // gift of howler
-				{ type: 'item', id: 19622 },
+				{ type: 'item', id: 19622},
 				{ type: 'item', id: 19640},
 				{ type: 'item', id: 19676, amount: 100},
 				{ type: 'item', id: 24618}
 			],
+			19664: [ // gift of (light?)
+				{type: 'currency', id: 'ascalonian_tear', amount: 500}
+			],
+			19665: [ // gift of the nobleman
+				{type: 'currency', id: 'seal_of_beetletun', amount: 500}
+			],
+			19666: [ // gift of the forgeman
+				{type: 'currency', id: 'manifesto_of_the_moletariate', amount: 500}
+			],
 			19667: [ // gift of thorns
 				{ type: 'currency', id: 'deadly_bloom', amount: 500 }
+			],
+			19668: [ // gift of baelfire
+				{ type: 'currency', id: 'flame_legion_charr_carving', amount: 500 }
+			],
+			19669: [ // gift of zhaitan
+				{type: 'currency', id: 'shard_of_zhaitan', amount: 500}
+			],
+			19670: [ // gift of sanctuary
+				{type: 'currency', id: 'symbol_of_koda', amount: 500}
+			],
+			19671: [ // gift of knowledge
+				{type: 'currency', id: 'knowledge_crystal', amount: 500}
 			],
 			19672: [ // gift of might
 				{ type: 'item', id: 24357, amount: 250 },
@@ -39,17 +288,17 @@ angular.module('legendarySearch', [
 				{ type: 'item', id: 19677},
 				{ type: 'item', id: 19678}
 			],
-			19675: [ // mystic clover
-				{ type: 'item', id: 19925},
-				{ type: 'item', id: 19976},
-				{ type: 'item', id: 19721},
-				{ type: 'item', id: 20796, amount: 2},
-			],
 			19676: [ // icy runestone
 				{type: 'currency', id: 'copper', amount: 10000}
 			],
 			19678: [ // gift of battle
 				{type: 'currency', id: 'badge_of_honor', amount: 500}
+			],
+			19675: [ // mystic clover
+				{ type: 'item', id: 19925},
+				{ type: 'item', id: 19976},
+				{ type: 'item', id: 19721},
+				{ type: 'item', id: 20796, amount: 2},
 			],
 			19925: [ // obsidian shard
 				{type: 'currency', id: 'karma', amount: 2100}
@@ -60,83 +309,95 @@ angular.module('legendarySearch', [
 			20797: [ // bloodstone shard
 				{type: 'currency', id: 'spirit_shard', amount: 200}
 			],
-			19623: [ // gift of energy
-				{type: 'item', id: 24274, amount: 250},
-				{type: 'item', id: 24275, amount: 250},
-				{type: 'item', id: 24276, amount: 250},
-				{type: 'item', id: 24277, amount: 250},
+			30684: [ // frostfang
+				{type: 'item', id: 29166},
+				{type: 'item', id: 19625},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19621: [ // gift of metal
-				{type: 'item', id: 19686, amount: 250},
-				{type: 'item', id: 19681, amount: 250},
-				{type: 'item', id: 19684, amount: 250},
-				{type: 'item', id: 19685, amount: 250},
+			30685: [ // kudzu
+				{type: 'item', id: 29172},
+				{type: 'item', id: 19644},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19622: [ // gift of wood
-				{type: 'item', id: 19714, amount: 250},
-				{type: 'item', id: 19711, amount: 250},
-				{type: 'item', id: 19709, amount: 250},
-				{type: 'item', id: 19712, amount: 250},
+			30686: [ // the dreamer
+				{type: 'item', id: 29178},
+				{type: 'item', id: 19660},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19638: [ // gift of color
-				{type: 'item', id: 19669, amount: 1},
-				{type: 'item', id: 20323, amount: 100},
-				{type: 'item', id: 24277, amount: 250},
-				{type: 'item', id: 24522, amount: 100}
+			30687: [ // incinerator
+				{type: 'item', id: 29167},
+				{type: 'item', id: 19645},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19639: [ // gift of lightning
-				{type: 'item', id: 19664, amount: 1},
-				{type: 'item', id: 19685, amount: 250},
-				{type: 'item', id: 19746, amount: 250},
-				{type: 'item', id: 24305, amount: 100}
+			30688: [ // the minstrel
+				{type: 'item', id: 29168},
+				{type: 'item', id: 19646},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19628: [ // unicorn statue
-				{type: 'item', id: 19667, amount: 1},
-				{type: 'item', id: 19685, amount: 250},
-				{type: 'item', id: 24512, amount: 100},
-				{type: 'item', id: 24522, amount: 100}
+			30689: [ // eternity
+				{type: 'item', id: 30703},
+				{type: 'item', id: 30704},
+				{type: 'item', id: 24277, amount: 5},
+				{type: 'item', id: 20796, amount: 10},
 			],
-			19629: [ // gift of history
-				{type: 'item', id: 19664, amount: 1},
-				{type: 'item', id: 19737, amount: 250},
-				{type: 'item', id: 24277, amount: 250},
-				{type: 'item', id: 24310, amount: 100}
+			30693: [ // quip
+				{type: 'item', id: 29174},
+				{type: 'item', id: 19651},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19669: [ // gift of zhaitan
-				{type: 'currency', id: 'shard_of_zhaitan', amount: 500}
+			30696: [ // the flameseeker prophecies
+				{type: 'item', id: 29177},
+				{type: 'item', id: 19653},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
-			19664: [
-				{type: 'currency', id: 'ascalonian_tear', amount: 500}
-			],
-			19654: [ // gift of the bifrost
-				{type: 'item', id: 19623, amount: 1},
-				{type: 'item', id: 19638, amount: 1},
-				{type: 'item', id: 19676, amount: 100},
-				{type: 'item', id: 24572, amount: 1},
-			],
-			19655: [ // gift of bolt
-				{type: 'item', id: 19621, amount: 1},
-				{type: 'item', id: 19639, amount: 1},
-				{type: 'item', id: 19676, amount: 100},
-				{type: 'item', id: 24554, amount: 1},
-			],
-			19660: [ // gift of the dreamer
-				{type: 'item', id: 19622, amount: 1},
-				{type: 'item', id: 19628, amount: 1},
-				{type: 'item', id: 19676, amount: 100},
-				{type: 'item', id: 24571, amount: 1},
-			],
-			19653: [ // gift of the flameseeker prophecies
-				{type: 'item', id: 19621, amount: 1},
-				{type: 'item', id: 19629, amount: 1},
-				{type: 'item', id: 19676, amount: 100},
-				{type: 'item', id: 24601, amount: 1},
+			30697: [ // frenzy
+				{type: 'item', id: 29179},
+				{type: 'item', id: 19659},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
 			30702: [ // howler
 				{type: 'item', id: 29184},
 				{type: 'item', id: 19662},
 				{type: 'item', id: 19626},
 				{type: 'item', id: 19674}
+			],
+			30690: [ // juggernaut
+				{type: 'item', id: 29170},
+				{type: 'item', id: 19649},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],
+			30691: [ // Kamohoali'i Kotaki
+				{type: 'item', id: 29171},
+				{type: 'item', id: 19657},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],
+			30692: [ // the moot
+				{type: 'item', id: 29173},
+				{type: 'item', id: 19650},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],
+			30694: [ // the predator
+				{type: 'item', id: 29175},
+				{type: 'item', id: 19661},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],	
+			30695: [ // meteorlogicus
+				{type: 'item', id: 29176},
+				{type: 'item', id: 19652},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
 			],
 			30698: [ // the bifrost
 				{type: 'item', id: 29180},
@@ -150,15 +411,27 @@ angular.module('legendarySearch', [
 				{type: 'item', id: 19626},
 				{type: 'item', id: 19674},
 			],
-			30686: [ // the dreamer
-				{type: 'item', id: 29178},
-				{type: 'item', id: 19660},
+			30700: [ // rodgort
+				{type: 'item', id: 29182},
+				{type: 'item', id: 19656},
 				{type: 'item', id: 19626},
 				{type: 'item', id: 19674},
 			],
-			30696: [ // the flameseeker prophecies
-				{type: 'item', id: 29177},
-				{type: 'item', id: 19653},
+			30701: [ // kraitkin
+				{type: 'item', id: 29183},
+				{type: 'item', id: 19658},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],
+			30703: [ // sunrise
+				{type: 'item', id: 29169},
+				{type: 'item', id: 19647},
+				{type: 'item', id: 19626},
+				{type: 'item', id: 19674},
+			],
+			30704: [ // twilight
+				{type: 'item', id: 29185},
+				{type: 'item', id: 19648},
 				{type: 'item', id: 19626},
 				{type: 'item', id: 19674},
 			]
@@ -168,7 +441,23 @@ angular.module('legendarySearch', [
 			30698,
 			30699,
 			30686,
-			30696
+			30696,
+			30697,
+			30684,
+			30687,
+			30690,
+			30685,
+			30701,
+			30691,
+			30695,
+			30688,
+			30692,
+			30694,
+			30693,
+			30700,
+			30703,
+			30704,
+			30689
 		]
 	};
 })
@@ -512,6 +801,9 @@ angular.module('legendarySearch', [
 				return {name: legendary.name, id: legendary.id};
 			})
 		})).then(function(availableLegendaries) {
+			availableLegendaries.sort(function(l1, l2) {
+				return l1.name.localeCompare(l2.name);
+			});
 			$scope.availableLegendaries = availableLegendaries;
 		});
 		$scope.selectedLegendary = null;
